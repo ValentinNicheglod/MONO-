f769a142312d5ac78add78f51029cc3b07bb82a2
Valentín Nicheglod <nicheglod69@gmail.com>
2021-02-25 17:51:30 -0300

    Updates

---
+++ precache-manifest.27a9f5a03544fd85276deff64d4fbe94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38c2a5eca92e1ddea1928662f94398b",
+    "url": "/MONO-/index.html"
+  },
+  {
+    "revision": "7cb9c1539e9def453ef0",
+    "url": "/MONO-/static/css/main.a624ed4f.chunk.css"
+  },
+  {
+    "revision": "b317c164365b6109eeea",
+    "url": "/MONO-/static/js/2.f199e71f.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/MONO-/static/js/2.f199e71f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cb9c1539e9def453ef0",
+    "url": "/MONO-/static/js/main.a8225667.chunk.js"
+  },
+  {
+    "revision": "88af78fdff040c545e79",
+    "url": "/MONO-/static/js/runtime-main.0e732826.js"
+  }
+]);
